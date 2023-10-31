5389bba6b5b25cd2607f05b462c6dfcd1c5eac80
mediamakermatt <mediamakermatt@gmail.com>
2023-10-31 14:21:50 -0400

    Remove console logging from CV since it's heavy

--- computerVision.js
+++ computerVision.js
@@ -1,3 +1,5 @@
+var debugAll = false;
+
 class LocalCache {
     valueCache = {}
     constructor() {
@@ -5,13 +7,13 @@ class LocalCache {
     }
     get(key) {
         if (this.valueCache[key] == undefined) {
-            console.log('adding to local cache', key, this.valueCache, this.valueCache[key], dw.get(key))
+            if(debugAll) console.log('adding to local cache', key, this.valueCache, this.valueCache[key], dw.get(key))
             this.valueCache[key] = dw.get(key) ?? null
         }
         return this.valueCache[key]
     }
     set(key, value) {
-        console.log('setting cache value', key, value)
+        if(debugAll) console.log('setting cache value', key, value)
         this.valueCache[key] = value
         dw.set(key, value)
     }
